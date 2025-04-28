d1431e681a73dd8ee7ddac26bf343b6d71b8b817
Emma Cadavra <emmacadavra@gmail.com>
2025-04-28 11:10:29 +0100

    Install node pg and connect to database

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 import express from "express";
+import { Client } from "pg";
+const client = new Client({
+  user: "admin",
+  password: "admin",
+  host: "localhost",
+  port: 5432,
+  database: "creature_feature",
+});
+await client.connect();
 
 const app = express();
 
@@ -6,4 +15,8 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(3000);
+app.get("/posts", (req, res) => {
+  return res.send("Hello World!");
+});
+
+app.listen(4000);
