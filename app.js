bfecd4d6627a2c9c780d8e7adcb1302423c47bd4
Emma Cadavra <emmacadavra@gmail.com>
2025-05-20 11:11:21 +0100

    Install Knex and Nodemon, connect to database and create first /posts query

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 import express from "express";
 import { Client } from "pg";
+import knex from "knex";
+
+const app = express();
+
+// PG Connection
 const client = new Client({
   user: "admin",
   password: "admin",
@@ -9,14 +14,42 @@ const client = new Client({
 });
 await client.connect();
 
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
+// Knex Connection
+const klient = knex({
+  client: "pg",
+  connection: {
+    user: "admin",
+    password: "admin",
+    host: "localhost",
+    port: 5432,
+    database: "creature_feature",
+  },
+  searchPath: ["creature_feature", "public"],
 });
 
-app.get("/posts", (req, res) => {
-  return res.send("Hello World!");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/posts", async (req, res) => {
+  const query = klient.select("posts_post.*").from("posts_post");
+
+  if (req.query.category) {
+    query.where("posts_post.category", req.query.category);
+  }
+
+  query.orderBy("posts_post.created_on", "desc");
+
+  // Debug: .toSQL().toNative()
+  const posts = await query;
+
+  res.send({
+    // hard-coded temporarily to match old API format
+    count: 3,
+    next: null,
+    previous: null,
+    results: posts,
+  });
 });
 
 app.listen(4000);
