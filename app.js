2e9b2ca9c4f41a60fb6351f25ae48190a6b8e93d
Emma Cadavra <emmacadavra@gmail.com>
2025-06-04 19:58:28 +0100

    Create comments mapper function and extend /comments query accordingly

--- app.js
+++ app.js
@@ -180,6 +180,7 @@ app.get("/posts", async (req, res) => {
   });
 });
 
+// POSTS MAPPER
 const postsMapper = async (posts, currentlyLoggedInUser) => {
   const postsArray = [];
 
@@ -227,11 +228,35 @@ app.get("/comments", async (req, res) => {
   }
 
   const query = klient
-    .select("comments_comment.*")
+    .select(
+      "comments_comment.*",
+      "auth_user.username AS comment_owner",
+      "profiles_profile.owner_id AS profile_id",
+      "profiles_profile.image AS profile_image",
+      "comment_likes.count AS likes_count"
+    )
     .from("comments_comment")
+    .innerJoin("auth_user", "comments_comment.owner_id", "auth_user.id")
+    .innerJoin(
+      "profiles_profile",
+      "comments_comment.owner_id",
+      "profiles_profile.owner_id"
+    )
+    .leftOuterJoin(
+      function () {
+        this.select("like_comments_likecomment.comment_id")
+          .count("like_comments_likecomment.id")
+          .from("like_comments_likecomment")
+          .groupBy("like_comments_likecomment.comment_id")
+          .as("comment_likes");
+      },
+      "comment_likes.comment_id",
+      "comments_comment.id"
+    )
     .where("comments_comment.post_id", req.query.post)
     .orderBy("comments_comment.created_on", "desc");
 
+  // Debug: .toSQL().toNative()
   const comments = await query;
 
   res.send({
@@ -239,8 +264,33 @@ app.get("/comments", async (req, res) => {
     count: 3,
     next: null,
     previous: null,
-    results: comments,
+    results: await commentsMapper(comments, req.query.currentlyLoggedInUser),
   });
 });
+
+// COMMENTS MAPPER
+const commentsMapper = async (comments, currentlyLoggedInUser) => {
+  const commentsArray = [];
+
+  for (const comment of comments) {
+    const profileImage = await getCloudinaryImage(comment.profile_image);
+
+    commentsArray.push({
+      id: comment.id,
+      owner: comment.comment_owner,
+      is_owner: Number(currentlyLoggedInUser) === comment.profile_id,
+      profile_id: comment.profile_id,
+      profile_image: profileImage,
+      like_id: null, // REDUNDANT
+      likes_count: comment.likes_count,
+      created_on: comment.created_on,
+      updated_on: comment.updated_on,
+      content: comment.content,
+      post: comment.post_id,
+    });
+  }
+
+  return commentsArray;
+};
 
 app.listen(4000);
